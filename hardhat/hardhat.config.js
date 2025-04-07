76ee77b07c2078cfca5d90d19f0a27c40f566e70
You Song Hou <ysongweb3@gmail.com>
2025-04-07 01:35:43 -0400

    feat: add Filecoin Calibration public RPC

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    calibration: {
+      url: "https://api.calibration.node.glif.io/rpc/v1",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
